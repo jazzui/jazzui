ad73b496d153bd3eb97832c4b6b7099bc52a2c90
Jared Forsyth <jared@jaredforsyth.com>
2013-11-29 11:21:22 -0700

    add optional basic auth

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var server = require('./lib/app')
   , DB = process.env.MONGO_URI || 'mongodb://localhost/jazzui'
   , PORT = process.env.PORT || '3000'
 
-server(DB).listen(PORT, function () {
+server(DB, process.env.JAZZUI_PASSWORD).listen(PORT, function () {
   console.log('Listening')
 })
 
